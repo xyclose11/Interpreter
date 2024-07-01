975a8e39ed35c49ff77395b10f9a9b00bda7b1bb
Harris Gustafson <harris.gustafson@outlook.com>
2024-06-30 20:08:13 -0500

    Init

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string // Using a string since debugging is easier compared to int/byte
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF" // Tells parser to stop "End Of File"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // Variables, function names etc
+	INT   = "INT"   // 1234567890
+
+	// Operators
+	ASSIGN = "="
+	PLUS   = "+"
+
+	// Delimiters
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookUpIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
