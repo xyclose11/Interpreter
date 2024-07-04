1192f46a0d4492f75709c6f276fe727a4f6b0d02
Harris Gustafson <harris.gustafson@outlook.com>
2024-07-04 07:51:21 -0500

    Added support for <>!/-*

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -30,10 +30,17 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
 
+	l.skipWhiteSpace()
+
 	switch l.ch {
 	default:
 		if isLetter(l.ch) {
 			tok.Literal = l.readIdentifier()
+			tok.Type = token.LookUpIdent(tok.Literal)
+			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
 			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
@@ -61,6 +68,24 @@ func (l *Lexer) NextToken() token.Token {
 	case '+':
 		tok = newToken(token.PLUS, l.ch)
 
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+
+	case '!':
+		tok = newToken(token.BANG, l.ch)
+
+	case '<':
+		tok = newToken(token.LT, l.ch)
+
+	case '>':
+		tok = newToken(token.GT, l.ch)
+
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -69,9 +94,27 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
+}
+
 func (l *Lexer) readIdentifier() string {
 	position := l.position
 	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhiteSpace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+	for isDigit(l.ch) {
 		l.readChar()
 	}
 	return l.input[position:l.position]
@@ -85,5 +128,7 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
-//TODO: readChar() add UTF-8/UNICODE support
-// Last on page 18
+//IP: readChar() add UTF-8/UNICODE support
+//readNumber() support floats, hex notation, octal, etc
+
+// ON PAGE 24
--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -8,13 +8,21 @@ import (
 func TestNextToken(t *testing.T) {
 	input := `let five = 5;
 
-	let 10 = 10;
+	let ten = 10;
 
 	let add = fn(x, y) {
 		x + y;
 	};
 
 	let result = add(five, ten);
+	!-/*5;
+	5 < 10 > 5;
+
+	if (5 < 10) {
+		return true;
+	} else {
+		return false;
+	}
 	`
 
 	tests := []struct {
@@ -30,6 +38,7 @@ func TestNextToken(t *testing.T) {
 		{token.IDENT, "ten"},
 		{token.ASSIGN, "="},
 		{token.INT, "10"},
+		{token.SEMICOLON, ";"},
 		{token.LET, "let"},
 		{token.IDENT, "add"},
 		{token.ASSIGN, "="},
@@ -49,12 +58,41 @@ func TestNextToken(t *testing.T) {
 		{token.LET, "let"},
 		{token.IDENT, "result"},
 		{token.ASSIGN, "="},
-		{token.FUNCTION, "add"},
+		{token.IDENT, "add"},
 		{token.LPAREN, "("},
 		{token.IDENT, "five"},
 		{token.COMMA, ","},
 		{token.IDENT, "ten"},
 		{token.RPAREN, ")"},
+		{token.SEMICOLON, ";"},
+		{token.BANG, "!"},
+		{token.MINUS, "-"},
+		{token.SLASH, "/"},
+		{token.ASTERISK, "*"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.GT, ">"},
+		{token.INT, "5"},
+		{token.SEMICOLON, ";"},
+		{token.IF, "if"},
+		{token.LPAREN, "("},
+		{token.INT, "5"},
+		{token.LT, "<"},
+		{token.INT, "10"},
+		{token.RPAREN, ")"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.TRUE, "true"},
+		{token.SEMICOLON, ";"},
+		{token.RBRACE, "}"},
+		{token.ELSE, "else"},
+		{token.LBRACE, "{"},
+		{token.RETURN, "return"},
+		{token.FALSE, "false"},
+		{token.SEMICOLON, ";"},
 		{token.RBRACE, "}"},
 		{token.EOF, ""},
 	}
